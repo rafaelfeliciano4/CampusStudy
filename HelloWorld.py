a272c7a11b3bebd798e4430e65ffb144a402676f
Rafael Feliciano <rafael.feliciano4@upr.edu>
2015-09-14 01:03:13 -0400

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+
+from flask import Flask
+import random
+from flask import render_template
+app = Flask(__name__)
+app.secret_key = 'This is really unique and secret'
+
+@app.route('/')
+
+
+
+def hello_person():
+    return render_template('Test.html')
+
+
+if __name__ == '__main__':
+    app.run()
